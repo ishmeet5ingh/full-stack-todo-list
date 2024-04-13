a18939d95ac366a6c0c88ee43886557a298106a6
ishmeet5ingh <ishmeet3j@gmail.com>
2024-04-13 16:31:35 +0530

    updated

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,8 +1,8 @@
 const conf = {
-    appwriteUrl: import.meta.env.VITE_APPWRITE_URL,
-    appwriteProjectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
-    appwriteDatabaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
-    appwriteCollectionId: import.meta.env.VITE_APPWRITE_COLLECTION_ID,
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
 }
 
 export default conf
